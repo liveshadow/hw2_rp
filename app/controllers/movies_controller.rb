7857b51cfa0878144db1fdd1210eff15029492a7
Olivia Abrant <olivia.abrant@colorado.edu>
2014-02-27 22:28:53 -0700

    Add session retention, filtering capability

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,13 +9,41 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
     @all_ratings = ['G','PG','PG-13','R','NC-17','NR'] #Array of all possible ratings to reference to
-    @checked = {}                                      #Empty array will be filled with whatever 
+    @checked = {}        #Empty array will be filled with checked values
+
+    #sorting stuff------------------------------------------------------------------------------
+    if params[:sort] == nil && session[:sort] != nil
+      params[:sort] = session[:sort]
+    end
 
     if(params[:sort].to_s == 'title')
     	@movies = @movies.sort_by {|mov| mov.title}
+      session[:sort] = params[:sort]
     elsif(params[:sort].to_s == 'release')
     	@movies = @movies.sort_by {|mov| mov.release_date.to_s}
+      session[:sort] = params[:sort]
     end
+
+    #rating filters-----------------------------------------------------------------------------
+    if (params[:ratings] == nil && session[:ratings] != nil) #case where user unchecks all boxes
+      params[:ratings] = session[:ratings]
+    end
+
+    if params[:ratings] != nil    #case where ratings params do exist
+      @all_ratings.each do |rating|
+        @checked[rating] = params[:ratings].has_key?(rating)
+      end
+    else    #case where no ratings params exist
+      @all_ratings.each do |rating|
+        @checked[rating] = true     #set all ratings to be checked
+      end
+    end
+    
+    if params[:ratings] != nil    #filter based on selected ratings
+      @movies = @movies.find_all{|m| params[:ratings].has_key?(m.rating)}
+      session[:ratings] = params[:ratings]
+    end
+
   end
 
   def new
